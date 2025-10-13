74dbc0cb46939a6128c182f0df30132f4e3fc249
TejasvSingh <singhtejasv98@gmail.com>
2025-10-13 13:04:02 -0500

    Initial Commit

---
+++ swiftUIPracticenewPlaylist/swiftUIPracticenewPlaylistApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftUIPracticenewPlaylistApp.swift
+//  swiftUIPracticenewPlaylist
+//
+//  Created by Tejasv Singh on 10/13/25.
+//
+
+import SwiftUI
+
+@main
+struct swiftUIPracticenewPlaylistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
